4842e823ed2aaa44f3951d4498cabacc8db253ce
Benjamin Krier <benkrier@gmail.com>
2017-05-02 14:37:37 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,80 @@
+function openText(evt, textType) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(textType).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+//function alterText() {
+//    var x = document.getElementById("originalText").value;
+
+    function ApplyLineBreaks(strTextarea) {
+        var oTextarea = document.getElementById(strTextarea);
+        if (oTextarea.wrap) {
+            oTextarea.setAttribute("wrap", "off");
+        }
+        else {
+            oTextarea.setAttribute("wrap", "off");
+            var newArea = oTextarea.cloneNode(true);
+            newArea.value = oTextarea.value;
+            oTextarea.parentNode.replaceChild(newArea, oTextarea);
+            oTextarea = newArea;
+        }
+
+        var strRawValue = oTextarea.value;
+        oTextarea.value = "";
+        var nEmptyWidth = oTextarea.scrollWidth;
+        var nLastWrappingIndex = -1;
+        for (var i = 0; i < strRawValue.length; i++) {
+            var curChar = strRawValue.charAt(i);
+            if (curChar == ' ' || curChar == '-' || curChar == '+')
+                nLastWrappingIndex = i;
+            oTextarea.value += curChar;
+            if (oTextarea.scrollWidth > nEmptyWidth) {
+                var buffer = "";
+                if (nLastWrappingIndex >= 0) {
+                    for (var j = nLastWrappingIndex + 1; j < i; j++)
+                        buffer += strRawValue.charAt(j);
+                    nLastWrappingIndex = -1;
+                }
+                buffer += curChar;
+                oTextarea.value = oTextarea.value.substr(0, oTextarea.value.length - buffer.length);
+                oTextarea.value += "\n" + buffer;
+            }
+        }
+        oTextarea.setAttribute("wrap", "");
+
+        x = oTextarea.value.replace(new RegExp("\\n", "g"), "<br />");
+
+        lines = x.split('<br />');
+
+        revLines = [];
+        for (e = 0; e < lines.length; e++) {
+          if (e % 2 === 0) {
+            revLines.push(lines[e]);
+          } else {
+            revLines.push(lines[e].split(' ').reverse().join(' '));
+          }
+        }
+
+        console.log(revLines);
+        y = revLines.join('<br />');
+
+        document.getElementById("alteredText").innerHTML = y;
+    }
+
+
+
+//    x = x.split(/\b/).reverse().join('');
+
+//    document.getElementById("alteredText").innerHTML = oTextarea.value;
+//}
