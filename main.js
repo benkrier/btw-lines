2b2323fc2b581fe6fc294ec00dfb60f9afc41100
Benjamin Krier <benkrier@gmail.com>
2017-05-22 11:18:51 -0500

    Add onclick event

--- main.js
+++ main.js
@@ -14,63 +14,60 @@ function openText(evt, textType) {
 
 document.getElementById("defaultOpen").click();
 
-//function alterText() {
-//    var x = document.getElementById("originalText").value;
-
-    function ApplyLineBreaks(strTextarea) {
-        var oTextarea = document.getElementById(strTextarea);
-        if (oTextarea.wrap) {
-            oTextarea.setAttribute("wrap", "off");
-        }
-        else {
-            oTextarea.setAttribute("wrap", "off");
-            var newArea = oTextarea.cloneNode(true);
-            newArea.value = oTextarea.value;
-            oTextarea.parentNode.replaceChild(newArea, oTextarea);
-            oTextarea = newArea;
-        }
-
-        var strRawValue = oTextarea.value;
-        oTextarea.value = "";
-        var nEmptyWidth = oTextarea.scrollWidth;
-        var nLastWrappingIndex = -1;
-        for (var i = 0; i < strRawValue.length; i++) {
-            var curChar = strRawValue.charAt(i);
-            if (curChar == ' ' || curChar == '-' || curChar == '+')
-                nLastWrappingIndex = i;
-            oTextarea.value += curChar;
-            if (oTextarea.scrollWidth > nEmptyWidth) {
-                var buffer = "";
-                if (nLastWrappingIndex >= 0) {
-                    for (var j = nLastWrappingIndex + 1; j < i; j++)
-                        buffer += strRawValue.charAt(j);
-                    nLastWrappingIndex = -1;
-                }
-                buffer += curChar;
-                oTextarea.value = oTextarea.value.substr(0, oTextarea.value.length - buffer.length);
-                oTextarea.value += "\n" + buffer;
-            }
-        }
-        oTextarea.setAttribute("wrap", "");
-
-        x = oTextarea.value.replace(new RegExp("\\n", "g"), "<br />");
-
-        lines = x.split('<br />');
-
-        revLines = [];
-        for (e = 0; e < lines.length; e++) {
-          if (e % 2 === 0) {
-            revLines.push(lines[e]);
-          } else {
-            revLines.push(lines[e].split(' ').reverse().join(' '));
-          }
-        }
-
-        console.log(revLines);
-        y = revLines.join('<br />');
-
-        document.getElementById("alteredText").innerHTML = y;
+function ApplyLineBreaks(strTextarea) {
+    var oTextarea = document.getElementById(strTextarea);
+    if (oTextarea.wrap) {
+        oTextarea.setAttribute("wrap", "off");
     }
+    else {
+        oTextarea.setAttribute("wrap", "off");
+        var newArea = oTextarea.cloneNode(true);
+        newArea.value = oTextarea.value;
+        oTextarea.parentNode.replaceChild(newArea, oTextarea);
+        oTextarea = newArea;
+    }
+
+    var strRawValue = oTextarea.value;
+    oTextarea.value = "";
+    var nEmptyWidth = oTextarea.scrollWidth;
+    var nLastWrappingIndex = -1;
+    for (var i = 0; i < strRawValue.length; i++) {
+        var curChar = strRawValue.charAt(i);
+        if (curChar == ' ' || curChar == '-' || curChar == '+')
+            nLastWrappingIndex = i;
+        oTextarea.value += curChar;
+        if (oTextarea.scrollWidth > nEmptyWidth) {
+            var buffer = "";
+            if (nLastWrappingIndex >= 0) {
+                for (var j = nLastWrappingIndex + 1; j < i; j++)
+                    buffer += strRawValue.charAt(j);
+                nLastWrappingIndex = -1;
+            }
+            buffer += curChar;
+            oTextarea.value = oTextarea.value.substr(0, oTextarea.value.length - buffer.length);
+            oTextarea.value += "\n" + buffer;
+        }
+    }
+    // oTextarea.setAttribute("wrap", "");
+
+    x = oTextarea.value.replace(new RegExp("\\n", "g"), "<br />");
+
+    lines = x.split('<br />');
+
+    revLines = [];
+    for (e = 0; e < lines.length; e++) {
+      if (e % 2 === 0) {
+        revLines.push(lines[e]);
+      } else {
+        revLines.push(lines[e].split(' ').reverse().join(' '));
+      }
+    }
+
+    console.log(revLines);
+    y = revLines.join('<br />');
+
+    document.getElementById("alteredText").innerHTML = y;
+}
 
 
 
